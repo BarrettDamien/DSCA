a00eb7dedd3dddac14279df2e6a13b91089315db
BarrettDamien <125761024+BarrettDamien@users.noreply.github.com>
2024-04-13 22:39:50 +0100

    EOD Commit
    Made several tweaks and build-outs following the course tutorials

--- client/app.js
+++ client/app.js
@@ -1,13 +1,19 @@
 var readlineSync = require("readline-sync")
+var readline = require("readline")
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/pollution.proto"
-var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-var pollution_proto = grpc.loadPackageDefinition(packageDefinition).DSCA
-
-var clientAir = new pollution_proto.AirPollutionService("0.0.0.0:40001", grpc.credentials.createInsecure())
-var clientWater = new pollution_proto.WaterPollutionService("0.0.0.0:40002", grpc.credentials.createInsecure())
-var clientWeather = new pollution_proto.WeatherStationService("0.0.0.0:40003", grpc.credentials.createInsecure())
+var AIR_PROTO_PATH = __dirname + "/protos/air_pollution.proto"
+var WATER_PROTO_PATH = __dirname + "/protos/water_pollution.proto"
+var WEATHER_PROTO_PATH = __dirname + "/protos/weather_station.proto"
+var packageDefinitionAir = protoLoader.loadSync(AIR_PROTO_PATH)
+var packageDefinitionWater = protoLoader.loadSync(WATER_PROTO_PATH)
+var packageDefinitionWeather = protoLoader.loadSync(WEATHER_PROTO_PATH)
+var air_pollution_proto = grpc.loadPackageDefinition(packageDefinitionAir).DSCA
+var water_pollution_proto = grpc.loadPackageDefinition(packageDefinitionWater).DSCA
+var weather_station_proto = grpc.loadPackageDefinition(packageDefinitionWeather).DSCA
+var clientAir = new air_pollution_proto.AirPollutionService("0.0.0.0:40001", grpc.credentials.createInsecure())
+var clientWater = new water_pollution_proto.WaterPollutionService("0.0.0.0:40002", grpc.credentials.createInsecure())
+var clientWeather = new weather_station_proto.WeatherStationService("0.0.0.0:40003", grpc.credentials.createInsecure())
 
 console.log("Select a service to use:")
 console.log("1. Air Pollution Service")
@@ -27,32 +33,134 @@ switch (choice) {
       switch (option1) {
         case 1:
           //Bidirectional Streaming RPC StreamAirPollutionData
-          var location = readlineSync.question("Please enter location. ")
-          var pollution_level = parseFloat(readlineSync.question("Please enter the pollution level for this location. "))
+          var call = clientWater.StreamAirPollutionData({location: location, pollution_level: pollution_level})
+          call.on('data', function(response){
+            console.log(response.location + " recorded a pollution level of " + response.pollution_level + ". " + response.message)
+          })
+          call.on('end', function(){
 
-          clientAir.StreamAirPollutionData({location: location, pollution_level: pollution_level}, function(error, response){
+          })
+          call.on('error', function(e){
+            console.log("An Error Occurred", e)
+          })
+          var location = readlineSync.question("Where is this survey for? ")
+          console.log("What is the pollution level? (Type q to quit)")
+          var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          })
+          rl.on('line', function(pollution_level){
+            if(pollution_level.toLowerCase === "q"){
+              call.end()
+              rl.close()
+            } else{
+              call.write({
+                location: location,
+                pollution_level: parseFloat(pollution_level)
+              })
+            }
+          })
+          break;
+
+        case 2:
+          //Server Streaming RPC GetHistoricalAirPollutionData
+          var location = readlineSync.question("Please enter location: ")
+          var days = parseInt(readlineSync.question("How many days of historical data do you wish to generate: "))
+          var call = clientAir.GetHistoricalAirPollutionData({location: location, days: days})
+
+          call.on('data', function(response) {
+            console.log("Record Data:  " + response.air_data)
+          })
+          call.on('end', function(){
+            console.log("Server Connection Ended")
+          })
+          call.on('error', function(e){
+            console.log("An Error Occurred", e)
+          })
+          break;
+
+        case 3:
+          //Unary RPC ConfigureAirSensorSettings
+          var location = readlineSync.question("Please enter location: ")
+          clientAir.ConfigureAirSensorSettings({location: location}, function(error, response){
             if(error){
               console.error("Error: ", error)
             } else{
               try {
-                if(response.message){
-                  console.log(response.message)
-                } else{
-                  console.log(response.success)
-                }
+                console.log("Result: " + response.result + " Last Inspection Date: " + response.inspection_Date)
               } catch (e) {
                 console.log("Could not connect to server", e)
               }
             }
           })
           break;
+
+        default:
+          console.log("Invalid choice. Please select from options 1, 2, or 3.")
+          break;
+        }
+        break;
+
+    case 2:
+      // Water Pollution Service
+      console.log("Select a function to use:")
+      console.log("1. Record Water Pollution Data")
+      console.log("2. View Historical Pollution Data")
+      console.log("3. Check Sensor")
+      var option2 = parseInt(readlineSync.question("Enter your number choice: "))
+
+      switch (option2){
+        case 1:
+          //Bidirectional Streaming RPC StreamWaterPollutionData
+          var call = clientWater.StreamWaterPollutionData()
+          call.on('data', function(response){
+            console.log(response.location + " recorded a pollution level of " + response.pollution_level + ". " + response.message)
+          })
+          call.on('end', function(){
+
+          })
+          call.on('error', function(e){
+            console.log("An Error Occurred", e)
+          })
+          var location = readlineSync.question("Where is this survey for? ")
+          console.log("What is the pollution level? (Type q to quit)")
+          var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          })
+          rl.on('line', function(pollution_level){
+            if(pollution_level.toLowerCase === "q"){
+              call.end()
+              rl.close()
+            } else{
+              call.write({
+                location: location,
+                pollution_level: parseFloat(pollution_level)
+              })
+            }
+          })
+          break;
+
         case 2:
-          //Server Streaming RPC GetHistoricalAirPollutionData
+          //Server Streaming RPC GetHistoricalWaterPollutionData
+          var location = readlineSync.question("Please enter location: ")
+          var days = parseInt(readlineSync.question("How many days of historical data do you wish to generate: "))
+          var call = clientWater.GetHistoricalWaterPollutionData({location: location, days: days})
+
+          call.on('data', function(response) {
+            console.log("Record Data:  " + response.water_data)
+          })
+          call.on('end', function(){
+            console.log("Server Connection Ended")
+          })
+          call.on('error', function(e){
+            console.log("An Error Occurred", e)
+          })
           break;
 
         case 3:
-          //Unary RPC ConfigureAirSensorSettings
-          clientAir.ConfigureAirSensorSettings({location: location}, function(error, response){
+          //Unary RPC ConfigureWaterSensorSettings
+          clientWater.ConfigureWaterSensorSettings({location: location}, function(error, response){
             if(error){
               console.error("Error: ", error)
             } else{
@@ -72,32 +180,6 @@ switch (choice) {
         default:
           console.log("Invalid choice. Please select from options 1, 2, or 3.")
           break;
-        }
-        break;
-    case 2:
-      // Water Pollution Service
-      console.log("Select a function to use:")
-      console.log("1. Record Water Pollution Data")
-      console.log("2. View Historical Pollution Data")
-      console.log("3. Check Sensor")
-      var option2 = parseInt(readlineSync.question("Enter your number choice: "))
-
-      switch (option2){
-        case 1:
-          //Bidirectional Streaming RPC StreamWaterPollutionData
-          break;
-
-        case 2:
-          //Server Streaming RPC GetHistoricalWaterPollutionData
-          break;
-
-        case 3:
-          //Unary RPC ConfigureWaterSensorSettings
-          break;
-
-        default:
-          console.log("Invalid choice. Please select from options 1, 2, or 3.")
-          break;
 
       }
       break;
@@ -112,14 +194,64 @@ switch (choice) {
       switch (option3){
         case 1:
             //Client Streaming RPC PublishWeatherData
+            var call = clientWeather.PublishWeatherData(function(error, response){
+              if(error){
+                console.log("An error occured")
+              } else {
+                console.log("From the input data, the average temperature is " + response.average_temperature + " and the average humidity is " + response.average_humidity)
+              }
+            })
+
+            var temperature_input
+            var humidity_input
+            var user_input
+
+            while(true){
+              temperature_input = readlineSync.question("Enter temperature: ")
+              humidity_input = readlineSync.question("Enter humidity: ")
+              user_input = readlineSync.question("End entries? Y/N ")
+              if(user_input.toLowerCase() === "y"){
+                break
+              }
+              call.write({ temperature: parseFloat(temperature_input), humidity: parseFloat(humidity_input)})
+            }
+            call.end()
             break;
 
         case 2:
-            //Server Streaming RPC GetHistoricalWaterPollutionData
+            //Server Streaming RPC GetHistoricalWeatherData
+            var location = readlineSync.question("Please enter location: ")
+            var days = parseInt(readlineSync.question("How many days of historical data do you wish to generate: "))
+            var call = clientAir.GetHistoricalWeatherData({location: location, days: days})
+
+            call.on('data', function(response) {
+              console.log("Record Data:  " + response.weather_data)
+            })
+            call.on('end', function(){
+              console.log("Server Connection Ended")
+            })
+            call.on('error', function(e){
+              console.log("An Error Occurred", e)
+            })
             break;
 
         case 3:
             //Unary RPC ConfigureWaterSensorSettings
+            clientWeather.ConfigureStationSettings({location: location}, function(error, response){
+              if(error){
+                console.error("Error: ", error)
+              } else{
+                try {
+                  if(response.message){
+                    console.log(response.message)
+                  } else{
+                    console.log("Result: " + response.result + " Last Inspection Date: " + response.inspection_Date)
+                  }
+                } catch (e) {
+                  console.log("Could not connect to server", e)
+                }
+              }
+            })
             break;
 
         default:
@@ -127,7 +259,7 @@ switch (choice) {
           break;
       }
       break;
-      
+
       default:
         console.log("Invalid choice. Please select from options 1, 2, or 3.")
   }
--- server/app.js
+++ server/app.js
@@ -1,73 +1,147 @@
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/pollution.proto"
-//var WATER_PROTO_PATH = __dirname + "/protos/water_pollution.proto"
-//var WEATHER_PROTO_PATH = __dirname + "/protos/weather_station.proto"
-var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-//var packageDefinitionWater = protoLoader.loadSync(WATER_PROTO_PATH)
-//var packageDefinitionWeather = protoLoader.loadSync(WEATHER_PROTO_PATH)
-var pollution_proto = grpc.loadPackageDefinition(packageDefinition).DSCA
-//var water_pollution_proto = grpc.loadPackageDefinition(packageDefinitionWater)
-//var weather_station_proto = grpc.loadPackageDefinition(packageDefinitionWeather)
-var today = new Date().toISOString().slice(0,10)
-//var inspection_Date = new Date(today)
-//inspection_Date.setDate(today.getDate()-45)
+var AIR_PROTO_PATH = __dirname + "/protos/air_pollution.proto"
+var WATER_PROTO_PATH = __dirname + "/protos/water_pollution.proto"
+var WEATHER_PROTO_PATH = __dirname + "/protos/weather_station.proto"
+var packageDefinitionAir = protoLoader.loadSync(AIR_PROTO_PATH)
+var packageDefinitionWater = protoLoader.loadSync(WATER_PROTO_PATH)
+var packageDefinitionWeather = protoLoader.loadSync(WEATHER_PROTO_PATH)
+var air_pollution_proto = grpc.loadPackageDefinition(packageDefinitionAir).DSCA
+var water_pollution_proto = grpc.loadPackageDefinition(packageDefinitionWater).DSCA
+var weather_station_proto = grpc.loadPackageDefinition(packageDefinitionWeather).DSCA
 
+//Unary Vars
+var today = new Date().toISOString().slice(0,10)
 var randomSuccess = () => Math.random() >= 0.5
 
+//Bidirection Vars
+var sensor_location = {}
+var pollution_metrics = {}
+var highest_pollution = 0
+var message = null
+
 //Air Pollution Functions
-function StreamAirPollutionData(call, callback){
-  try {
-    var location = parse(call.request.location)
-    var pollution_level = parseFloat(call.request.pollution_level)
-    if(isNaN(location) && !isNaN(pollution_level)){
-      var success = "Data Received and Logged"
-      callback(null, {
-        message: undefined,
-        success: success
-      })
-    } else{
-      callback(null, {
-        message: "Please enter a valid location and pollution level"
+function StreamAirPollutionData(call){
+  call.on('data', function(data){
+    const {location, pollution_level} = data
+    if(!(pollution_level.location in sensor_location)){
+      sensor_location[pollution_level.location] = {
+        location: pollution_level.location,
+        call: call
+      }
+    }
+    if(!(pollution_level.location in sensor_location)){
+      sensor_location[pollution_level.location] = 0
+    }
+    sensor_location[pollution_level.location] += 1
+    if(pollution_level.pollution_level > highest_pollution || !message){
+      highest_pollution = pollution_level.pollution_level
+      message = "Highest recorded pollution level is in " + pollution_level.location
+    }
+    for(var sensor in sensor_location){
+      sensor_location[sensor].call.write({
+        pollution_level: pollution_level.pollution_level,
+        location: pollution_level.location,
+        message: message
       })
     }
+  })
+  call.on('end', function(){
+    call.end()
+  })
+  call.on('error', function(e){
+    console.log(e)
+  })
+}
+
+function GetHistoricalAirPollutionData(call){
+  try {
+    // var location = call.request.location
+    // var days = parseInt(call.request.days)
+    const {location, days} = call.request
+    for(var i = 0; i < days; i++){
+      var air_data = Math.round(Math.random() * 25) + 5;
+      call.write({
+        air_data: air_data
+      })
+    }
+    call.end()
+
   } catch(e) {
     callback(null, {
       message: "An error occured"
     })
   }
-}
-
-function GetHistoricalAirPollutionData(call, callback){
-
 }
 
 function ConfigureAirSensorSettings(call, callback){
-  try {
-    var location = parse(call.request.location)
-    if(isNaN(location) && randomSuccess == true){
-      var result = "Sensor Online"
-      var inspection_Date = new Date(today)
-      inspection_Date.setDate(today.getDate()-15)
-      callback(null, {
-        message: undefined,
-        success: result,
-        last_inspection: inspection_Date.toISOString().slice(0,10)
-      })
-    } else if(isNaN(location) && randomSuccess == false){
-      var result = "Sensor Offline"
-      var inspection_Date = new Date(today)
-      inspection_Date.setDate(today.getDate()-45)
-      callback(null, {
-        message: undefined,
-        success: result,
-        last_inspection: inspection_Date.toISOString().slice(0,10)
-      })
-    } else {
-      callback(null, {
-        message: "Please enter a valid location"
+    const {location} = call.request
+    const last_inspection = "09/04/2024"
+    const result = randomSuccess() ? "Sensor Online" : "Sensor Offline"
+    callback(null, {result, last_inspection})
+  //   if(randomSuccess()){
+  //     var result = "Sensor Online"
+  //     // var inspection_Date = new Date(today);
+  //     // inspection_Date.setDate(inspection_Date.getDate()-15)
+  //     callback(null, {result: result, last_inspection: last_inspection})
+  //       //inspection_Date.toISOString().slice(0,10)
+  //
+  //   } else if(!randomSuccess()){
+  //     var result = "Sensor Offline"
+  //     // var inspection_Date = new Date(today);
+  //     // inspection_Date.setDate(inspection_Date.getDate()-45)
+  //     callback(null, {result: result, last_inspection: last_inspection})
+  //   } else {
+  //     callback(null, {message: "Please enter a valid location"})
+  //   }
+  // }
+}
+
+//Water Pollution Functions
+function StreamWaterPollutionData(call){
+  call.on('data', function(data){
+    const {location, pollution_level} = data
+    if(!(pollution_level.location in sensor_location)){
+      sensor_location[pollution_level.location] = {
+        location: pollution_level.location,
+        call: call
+      }
+    }
+    if(!(pollution_level.location in sensor_location)){
+      sensor_location[pollution_level.location] = 0
+    }
+    sensor_location[pollution_level.location] += 1
+    if(pollution_level.pollution_level > highest_pollution || !message){
+      highest_pollution = pollution_level.pollution_level
+      message = "Highest recorded pollution level is in " + pollution_level.location
+    }
+    for(var sensor in sensor_location){
+      sensor_location[sensor].call.write({
+        pollution_level: pollution_level.pollution_level,
+        location: pollution_level.location,
+        message: message
       })
     }
+  })
+  call.on('end', function(){
+    call.end()
+  })
+  call.on('error', function(e){
+    console.log(e)
+  })
+}
+
+function GetHistoricalWaterPollutionData(call, callback){
+  try {
+    const {location, days} = call.request
+    for(var i = 0; i < days; i++){
+      var water_data = Math.round(Math.random() * 25) + 5;
+      call.write({
+        water_data: water_data
+      })
+    }
+    call.end()
+
   } catch(e) {
     callback(null, {
       message: "An error occured"
@@ -75,49 +149,76 @@ function ConfigureAirSensorSettings(call, callback){
   }
 }
 
-//Water Pollution Functions
-function StreamWaterPollutionData(call, callback){
-
-}
-
-function GetHistoricalWaterPollutionData(call, callback){
-
-}
-
 function ConfigureWaterSensorSettings(call, callback){
-
+  const {location} = call.request
+  const last_inspection = "09/04/2024"
+  const result = randomSuccess() ? "Sensor Online" : "Sensor Offline"
+  callback(null, {result, last_inspection})
 }
 
 //Weather Station Functions
 function PublishWeatherData(call, callback){
+  var count = 0
+  var temp_sum = 0
+  var humid_sum = 0
 
+  call.on('data', function(request){
+    temp_sum += request.temperature
+    humid_sum += request.humidity
+    count += 1
+  })
+  call.on('end', function(){
+    callback(null, {average_temperature: temp_sum/count, average_humidity: humid_sum/count})
+  })
+  call.on('error', function(e){
+    console.log("An error occured")
+  })
 }
 
 function GetHistoricalWeatherData(call, callback){
+  try {
+    var location = call.request.location
+    var days = parseInt(call.request.days)
 
+    for(var i = 0; i < days; i++){
+      var weather_data = Math.round(Math.random() * 25) + 5;
+      call.write({
+        weather_data: weather_data
+      })
+    }
+    call.end()
+
+  } catch(e) {
+    callback(null, {
+      message: "An error occured"
+    })
+  }
 }
 
 function ConfigureStationSettings(call, callback){
-
+  const {location} = call.request
+  const last_inspection = "09/04/2024"
+  const result = randomSuccess() ? "Sensor Online" : "Sensor Offline"
+  callback(null, {result, last_inspection})
 }
 
 //Create Air Pollution Server
 var serverAir = new grpc.Server()
-serverAir.addService(pollution_proto.AirPollutionService.service, {StreamAirPollutionData: StreamAirPollutionData, GetHistoricalAirPollutionData: GetHistoricalAirPollutionData, ConfigureAirSensorSettings: ConfigureAirSensorSettings})
+serverAir.addService(air_pollution_proto.AirPollutionService.service, {StreamAirPollutionData: StreamAirPollutionData, GetHistoricalAirPollutionData: GetHistoricalAirPollutionData, ConfigureAirSensorSettings: ConfigureAirSensorSettings})
 serverAir.bindAsync("0.0.0.0:40001", grpc.ServerCredentials.createInsecure(), function() {
   serverAir.start()
 })
 
 //Create Water Pollution Server
 var serverWater = new grpc.Server()
-serverWater.addService(pollution_proto.WaterPollutionService.service, {StreamWaterPollutionData: StreamWaterPollutionData, GetHistoricalWaterPollutionData: GetHistoricalWaterPollutionData, ConfigureWaterSensorSettings: ConfigureWaterSensorSettings})
+serverWater.addService(water_pollution_proto.WaterPollutionService.service, {StreamWaterPollutionData: StreamWaterPollutionData, GetHistoricalWaterPollutionData: GetHistoricalWaterPollutionData, ConfigureWaterSensorSettings: ConfigureWaterSensorSettings})
 serverWater.bindAsync("0.0.0.0:40002", grpc.ServerCredentials.createInsecure(), function() {
   serverWater.start()
 })
 
 //Create Weather Station Server
 var serverWeather = new grpc.Server()
-serverWeather.addService(pollution_proto.WeatherStationService.service, {PublishWeatherData: PublishWeatherData, GetHistoricalWeatherData: GetHistoricalWeatherData, ConfigureStationSettings: ConfigureStationSettings})
+serverWeather.addService(weather_station_proto.WeatherStationService.service, {PublishWeatherData: PublishWeatherData, GetHistoricalWeatherData: GetHistoricalWeatherData, ConfigureStationSettings: ConfigureStationSettings})
 serverWeather.bindAsync("0.0.0.0:40003", grpc.ServerCredentials.createInsecure(), function() {
   serverWeather.start()
 })
