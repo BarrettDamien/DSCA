53bd5e146c4f2528678bf6374719a98b32db4507
BarrettDamien <125761024+BarrettDamien@users.noreply.github.com>
2024-04-13 15:37:04 +0100

    Functions skeleton created

--- server/app.js
+++ server/app.js
@@ -10,15 +10,62 @@ var pollution_proto = grpc.loadPackageDefinition(packageDefinition).DSCA
 //var water_pollution_proto = grpc.loadPackageDefinition(packageDefinitionWater)
 //var weather_station_proto = grpc.loadPackageDefinition(packageDefinitionWeather)
 
-function ???(call, callback){
-  try{
-    var =
-    var =
+//Air Pollution Functions
+function StreamAirPollutionData(call, callback){
 
 }
 
-var server = new grpc.Server()
-server.addService(pollution_proto.PollutionService.service, {???})
-server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
-  server.start()
+function GetHistoricalAirPollutionData(call, callback){
+
+}
+
+function ConfigureAirSensorSettings(call, callback){
+
+}
+
+//Water Pollution Functions
+function StreamWaterPollutionData(call, callback){
+
+}
+
+function GetHistoricalWaterPollutionData(call, callback){
+
+}
+
+function ConfigureWaterSensorSettings(call, callback){
+
+}
+
+//Weather Station Functions
+function PublishWeatherData(call, callback){
+
+}
+
+function GetHistoricalWeatherData(call, callback){
+
+}
+
+function ConfigureStationSettings(call, callback){
+
+}
+
+//Create Air Pollution Server
+var serverAir = new grpc.Server()
+serverAir.addService(pollution_proto.AirPollutionService.service, {StreamAirPollutionData: StreamAirPollutionData, GetHistoricalAirPollutionData: GetHistoricalAirPollutionData, ConfigureAirSensorSettings: ConfigureAirSensorSettings})
+serverAir.bindAsync("0.0.0.0:40001", grpc.ServerCredentials.createInsecure(), function() {
+  serverAir.start()
+})
+
+//Create Water Pollution Server
+var serverWater = new grpc.Server()
+serverWater.addService(pollution_proto.WaterPollutionService.service, {StreamWaterPollutionData: StreamWaterPollutionData, GetHistoricalWaterPollutionData: GetHistoricalWaterPollutionData, ConfigureWaterSensorSettings: ConfigureWaterSensorSettings})
+serverWater.bindAsync("0.0.0.0:40002", grpc.ServerCredentials.createInsecure(), function() {
+  serverWater.start()
+})
+
+//Create Weather Station Server
+var serverWeather = new grpc.Server()
+serverWeather.addService(pollution_proto.WeatherStationService.service, {PublishWeatherData: PublishWeatherData, GetHistoricalWeatherData: GetHistoricalWeatherData, ConfigureStationSettings: ConfigureStationSettings})
+serverWeather.bindAsync("0.0.0.0:40003", grpc.ServerCredentials.createInsecure(), function() {
+  serverWeather.start()
 })
